0009d1c56b3ac2627fbf359bf7adbb2c55768ac5
Damian Krzeminski <pirxpilot@furkot.com>
2023-09-19 09:30:03 +0200

    replace tape with node:test

--- test/import.js
+++ test/import.js
@@ -1,46 +1,48 @@
-const test = require('tape');
+const test = require('node:test');
+const assert = require('node:assert/strict');
+
 const fs = require('fs');
 const parse = require('..');
 
-test('should parse kmz', function (t) {
+test('should parse kmz', function (t, done) {
   const stream = fs.createReadStream(`${__dirname}/fixtures/usa.kmz`);
   parse(stream, (err, trip) => {
     const expected = require('./fixtures/usa.json');
 
-    t.error(err);
-    t.deepEqual(trip, expected);
-    t.end();
+    assert.ifError(err);
+    assert.deepEqual(trip, expected);
+    done();
   });
 });
 
-test('should raise error on a file that contains invalid KML', function (t) {
+test('should raise error on a file that contains invalid KML', function (t, done) {
   const stream = fs.createReadStream(`${__dirname}/fixtures/invalid-kml-inside.kmz`);
   parse(stream, (err, trip) => {
-    t.notLooseEqual(err, null, 'error should exists');
-    t.equal(err.err, 'invalid');
-    t.equal(err.message, 'Unexpected close tag');
-    t.looseEqual(trip, null, 'trip should not exist');
-    t.end();
+    assert.ok(err, 'error should exists');
+    assert.equal(err.err, 'invalid');
+    assert.equal(err.message, 'Unexpected close tag');
+    assert.ok(!trip, 'trip should not exist');
+    done();
   });
 });
 
-test('should raise error on a file that does not contain KML', function (t) {
+test('should raise error on a file that does not contain KML', function (t, done) {
   const stream = fs.createReadStream(`${__dirname}/fixtures/no-kml-inside.kmz`);
   parse(stream, (err, trip) => {
-    t.notLooseEqual(err, null, 'error should exists');
-    t.equal(err, 'invalid');
-    t.looseEqual(trip, null, 'trip should not exist');
-    t.end();
+    assert.ok(err, 'error should exists');
+    assert.equal(err, 'invalid');
+    assert.ok(!trip, 'trip should not exist');
+    done();
   });
 });
 
-test('should raise error on a file that cannot be unzipped', function (t) {
+test('should raise error on a file that cannot be unzipped', function (t, done) {
   const stream = fs.createReadStream(`${__dirname}/fixtures/not-a-zip.kmz`);
   parse(stream, (err, trip) => {
-    t.notLooseEqual(err, null, 'error should exists');
-    t.equal(err.err, 'invalid');
-    t.ok('message' in err, 'should have property message');
-    t.looseEqual(trip, null, 'trip should not exist');
-    t.end();
+    assert.ok(err, 'error should exists');
+    assert.equal(err.err, 'invalid');
+    assert.ok('message' in err, 'should have property message');
+    assert.ok(!trip, 'trip should not exist');
+    done();
   });
 });
